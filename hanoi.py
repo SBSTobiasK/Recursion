9d414d54a7a52076f1cf2815c07f6623c3f3e9d9
tobias <sbs202223kerchner@gmail.com>
2023-02-12 00:16:22 +0100

    binary?

--- hanoi.py
+++ hanoi.py
@@ -1,14 +1,20 @@
+loop = 0
+
 def move(f, t):
     print("Move {} to {}.".format(f,t))
 
 def hanoi(n, f, v, t):
+    global loop
     if n==0:
         pass
     else:
         hanoi((n-1), f, t, v)
         move(f,t)
+        loop += 1
         hanoi((n-1), v, f, t)
 
-hanoi(2, "A","B","C")
+hanoi(7, "A","B","C")
+print(loop)
 
 #https://www.youtube.com/watch?v=8lhxIOAfDss
+#https://www.youtube.com/watch?v=2SUvWfNJSsM
